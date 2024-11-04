a1b3fbf5d137cd3e171350b400f03c11542ae92c
Aleksii Nikitin <mckoda09@gmail.com>
2024-11-04 23:25:38 +0500

    v2

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    svelte(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  devToolbar: { enabled: false },
+});
