009cdcc08366cfc599e4fad3d975773d2d0475ee
Aleksii Nikitin <mckoda09@gmail.com>
2024-11-06 01:01:08 +0500

    made something

--- src/services/pocketbase.ts
+++ src/services/pocketbase.ts
@@ -1,3 +1,11 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("https://kvantorium-perm.pockethost.io");
+export const pb = new PocketBase(
+  import.meta.env.POCKETBASE_URL || "http://127.0.0.1:8090"
+);
+
+export interface Author {
+  firstName: string;
+  lastName: string;
+  birthday: Date;
+}
